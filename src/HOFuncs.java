0dac881dd9a131f626b0dc9258532874857dcbea
Carlos M <carlosjmarinj@gmail.com>
2019-12-19 11:30:21 -0600

    Higher order function simple example

---
+++ src/HOFuncs.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+    public class HOFuncs {
+        public static void main (String[] args){
+            List carNames = Arrays.asList("lamborghini", "ferrari", "porsche", "tesla");
+           // Collections.sort(carNames, (first, second) -> first.length()-second.length());
+            System.out.println(carNames);
+        }
+    }
